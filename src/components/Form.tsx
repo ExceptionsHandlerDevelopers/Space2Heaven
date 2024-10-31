2dfafccd93691d7bfc21b6b99b37bc671ae0eeec
Arif Basha K. <arifkolimi08@gmail.com>
2024-10-31 22:37:57 +0530

    Space2Haven - Implemented auth page

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,22 +1,77 @@
+"use client"
+import { useState } from "react"
 import Input from "./Input"
 
 const Form = ({ formType = "signin" }: { formType: string }) => {
+    const [authDetails, setAuthDetails] = useState({
+        name: "",
+        email: "",
+        password: "",
+        phoneNo: "",
+    })
+    const [confirmPassword, setConfirmPassword] = useState("")
+
+    const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const { name, value } = e.target
+        setAuthDetails(prevDetails => ({ ...prevDetails, [name]: value }))
+    }
+
+    const submitDetails = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        console.log(authDetails)
+    }
+
     return (
-        <form>
+        <form onSubmit={submitDetails}>
             {formType === "signup" && (
-                <Input title={"Full Name"} value="" placeholder="Enter your name" type={"text"} />
+                <>
+                    <Input
+                        title="Full Name"
+                        value={authDetails.name}
+                        placeholder="Enter your name"
+                        type="text"
+                        name="name"
+                        onChange={handleInputChange}
+                    />
+
+                    <Input
+                        title="Phone No"
+                        value={authDetails.phoneNo}
+                        placeholder="Enter your Phone No"
+                        type="text"
+                        name="phoneNo"
+                        onChange={handleInputChange}
+                    />
+                </>
             )}
 
+            <Input
+                title="Email ID"
+                value={authDetails.email}
+                placeholder="Enter your Email ID"
+                type="email"
+                name="email"
+                onChange={handleInputChange}
+            />
+
+            <Input
+                title="Password"
+                value={authDetails.password}
+                placeholder="Enter your password"
+                type="password"
+                name="password"
+                onChange={handleInputChange}
+            />
+
             {formType === "signup" && (
-                <Input title={"Phone No"} value="" placeholder="Enter your Phone No" type={"text"} />
-            )}
-
-            <Input title={"Email ID"} value="" placeholder="Enter your EmailID" type={"email"} />
-
-            <Input title={"Password"} value="" placeholder="Enter your password" type={"password"} />
-
-            {formType === "signup" && (
-                <Input title={"Confirm Password"} value="" placeholder="Confirm Password" type={"password"} />
+                <Input
+                    title="Confirm Password"
+                    value={confirmPassword}
+                    placeholder="Confirm Password"
+                    type="password"
+                    name="confirmPassword"
+                    onChange={(e) => setConfirmPassword(e.target.value)}
+                />
             )}
 
             <button type="submit" className="btn-class">
@@ -25,4 +80,5 @@ const Form = ({ formType = "signin" }: { formType: string }) => {
         </form>
     )
 }
+
 export default Form
